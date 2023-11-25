eb1113d300a379a539521d609a41e9a70475ddae
Noah Buss <nbuss@students.ntc.edu>
2023-11-25 10:28:39 -0600

    Added tests

---
+++ Sol.Test/Tests/Commands/ChangeItemStatusCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sol.Domain.Commands;
+using Sol.Test.Helpers;
+using Sol.Test.TestDoubles;
+
+namespace Sol.Test.Tests.Commands
+{
+    [TestClass]
+    public class ChangeItemStatusCommandTests
+    {
+        private readonly TestHobbyFile testHobbyFile;
+
+        private readonly ChangeItemStatusCommandHandler underTest;
+
+        public ChangeItemStatusCommandTests()
+        {
+            testHobbyFile = new(ItemHelper.GenerateItem("New Item", itemStatus: Domain.Models.ItemStatus.NotStarted));
+
+            underTest = new();
+        }
+
+        [TestMethod]
+        public async Task ItemIsSaved()
+        {
+            await underTest.ExecuteAsync(new(testHobbyFile, testHobbyFile.Items.First(), Domain.Models.ItemStatus.Complete));
+            Assert.IsTrue(testHobbyFile.UpdatedItems.Any());
+        }
+
+        [TestMethod]
+        public async Task ListIsOrganized()
+        {
+            await underTest.ExecuteAsync(new(testHobbyFile, testHobbyFile.Items.First(), Domain.Models.ItemStatus.Complete));
+            Assert.IsTrue(testHobbyFile.Organized);
+        }
+
+        [TestMethod]
+        public async Task StatusIsUpdated()
+        {
+            await underTest.ExecuteAsync(new(testHobbyFile, testHobbyFile.Items.First(), Domain.Models.ItemStatus.Complete));
+            Assert.IsTrue(testHobbyFile.UpdatedItems.First().Status == Domain.Models.ItemStatus.Complete);
+        }
+    }
+}
